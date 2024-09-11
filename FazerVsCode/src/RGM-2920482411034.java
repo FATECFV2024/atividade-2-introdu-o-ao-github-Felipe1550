5c9032c718994aa418d759aecfc1eadba807500b
Felipe1550 <fesschueller@gmail.com>
2024-09-10 23:10:49 -0300

    Atividade
    Aqui eu coloquei os dados pedidos na atividade

---
+++ FazerVsCode/src/RGM-2920482411034.java
@@ -0,0 +1,20 @@
+public class RA2920482411034 {
+
+     // Aqui eu guardo meu nome
+    public String Nome = "Felipe Schueller Araujo da Silva";
+
+    // Aqui eu guardo meu RA
+    public String RA = "2920482411034";
+
+    // Aqui eu guardo minha idade
+    public String Idade = "18 anos";
+
+    // Aqui eu guardo o link do meu Github
+    public String Github = "https://github.com/Felipe1550";
+
+    // Aqui eu guardo o link do meu LinkedIn
+    public String Linkedin = "https://www.linkedin.com/in/felipeschueller/";
+
+    // Aqui eu guardo minha turma
+    public String Turma = "2° ADS";
+}
